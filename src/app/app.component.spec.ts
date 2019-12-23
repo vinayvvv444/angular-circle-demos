29b194ceab6c78a991f297d9777dc758442431a1
Vinay Valsan <vinayvvv444@gmail.com>
2019-12-23 21:42:24 +0400

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-circle-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-circle-demo');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-circle-demo app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to circle demo');
   });
 });
